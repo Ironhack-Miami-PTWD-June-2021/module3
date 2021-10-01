8b04a2dca265b473073b906cf7e3dd32dc28c1df
Cule219 <culaficstefan@gmail.com>
2021-09-30 19:19:12 -0600

    react intro w webpack/babel
    create react app intro

---
+++ react-using-npm/src/app.js
@@ -0,0 +1,5 @@
+import React from "react"; // const React = require('react')
+
+const app = () => <h1>This is out title</h1>;
+
+export default app;
---
+++ react-using-npm/src/index.js
@@ -0,0 +1,5 @@
+import React from "react"; // const React = require('react')
+import ReactDOM from "react-dom";
+import App from "./app";
+
+ReactDOM.render(<App />, document.getElementById("root"));
