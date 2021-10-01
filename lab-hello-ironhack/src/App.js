41bd95778a39a57216f4c016a579918396c26212
Cule219 <culaficstefan@gmail.com>
2021-09-30 20:01:51 -0600

    lab-base

---
+++ lab-hello-ironhack/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="images/icon1.png" alt="icon" />
+      <img src="images/icon2.png" alt="icon" />
+    </div>
+  );
+}
+
+export default App;
